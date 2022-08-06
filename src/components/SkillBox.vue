30dbc456bbbb45be7f208f33fa48e9b486dc6ee8
zeoxo <dyldougherty@gmail.com>
2022-08-06 15:11:52 -0400

    moved skills into their own components

---
+++ src/components/SkillBox.vue
@@ -0,0 +1,81 @@
+<template>
+    <a class="skill" :href="link" target="_blank">
+      <img :src="icon" alt="skill icon">
+      <h3>{{text}}</h3>
+      <p class="time">{{years}} years</p>
+    </a>
+</template>
+
+<script setup lang="ts">
+import {computed} from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  startYear: {
+    type: Number,
+    default: 2001,
+    required: true,
+  },
+  link: {
+    type: String,
+    default: '',
+    required: true,
+  }
+})
+
+const years = computed(() => {
+  const endDate = new Date();
+  const diffTime = endDate.getFullYear() - props.startYear;
+  return diffTime;
+})
+</script>
+
+
+<style scoped>
+.skill {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: var(--col-old-pencil);
+  margin: 10px;
+  width: 150px;
+  height: 150px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: var(--col-old-paper);
+  transition: all 0.2s;
+  cursor: pointer;
+}
+
+.time {
+  color: var(--col-orange);
+}
+
+@media (hover: hover) {
+  .skill:hover {
+    box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
+    transform: scale(1.1);
+    color: var(--col-old-paper) !important;
+  }
+}
+
+img {
+  max-width: 50px;
+}
+
+h3 {
+  margin-top: 8px;
+}
+
+</style>
--- src/components/sections/Skills.vue
+++ src/components/sections/Skills.vue
@@ -4,65 +4,24 @@
   <h1>Skills</h1>
   <p>I love web development and the power it brings to all platforms. Write once, deploy anywhere has really spoken to me so I've focused a majority of my studied on web technology. Game development is another one of my great interests, I've used Unity for years, but recently switched to the Godot Engine due to its open source model.</p>
   <div class="list">
-    <a class="skill" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">
-      <img src="@/assets/svg/skills/html.svg" alt="">
-      <h3>HTML</h3>
-      <p class="time">6 years</p>
-    </a>
-    <a class="skill" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
-      <img src="@/assets/svg/skills/css.svg" alt="">
-      <h3>CSS</h3>
-      <p class="time">6 years</p>
-    </a>
-    <a class="skill" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="@/assets/svg/skills/javascript.svg" alt="">
-      <h3>JavaScript</h3>
-      <p class="time">6 years</p>
-    </a>
-    <a class="skill" href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/svg/skills/vue.svg" alt="">
-      <h3>Vue</h3>
-      <p class="time">5 years</p>
-    </a>
-    <a class="skill"  href="https://firebase.google.com/" target="_blank">
-      <img src="@/assets/svg/skills/firebase.svg" alt="">
-      <h3>Firebase</h3>
-      <p class="time">5 years</p>
-    </a>
-    <a class="skill" href="https://nodejs.org/en/" target="_blank">
-      <img src="@/assets/svg/skills/node.svg" alt="">
-      <h3>Node</h3>
-      <p class="time">5 years</p>
-    </a>
-    <a class="skill" href="https://git-scm.com/" target="_blank">
-      <img src="@/assets/svg/skills/git.svg" alt="">
-      <h3>Git</h3>
-      <p class="time">6 years</p>
-    </a>
-    <a class="skill" href="https://docs.microsoft.com/en-us/dotnet/csharp/" target="_blank">
-      <img src="@/assets/svg/skills/cSharp.svg" alt="">
-      <h3>C#</h3>
-      <p class="time">5 years</p>
-    </a>
-    <a class="skill" href="https://unity.com/" target="_blank">
-      <img src="@/assets/svg/skills/unity.svg" alt="">
-      <h3>Unity</h3>
-      <p class="time">5 years</p>
-    </a>
-    <a class="skill" href="https://godotengine.org/" target="_blank">
-      <img src="@/assets/svg/skills/godot.svg" alt="">
-      <h3>Godot</h3>
-      <p class="time">1 year</p>
-    </a>
-    <a class="skill" href="https://affinity.serif.com/en-us/designer/" target="_blank">
-      <img src="@/assets/svg/skills/affinityDesigner.svg" alt="">
-      <h3>Affinity</h3>
-      <p class="time">4 years</p>
-    </a>
+    <skillBox text="HTML" icon="src/assets/svg/skills/html.svg" :startYear="2016" link="https://developer.mozilla.org/en-US/docs/Web/HTML"></skillBox>
+    <skillBox text="CSS" icon="src/assets/svg/skills/css.svg" :startYear="2016" link="https://developer.mozilla.org/en-US/docs/Web/CSS"></skillBox>
+    <skillBox text="Javascript" icon="src/assets/svg/skills/javascript.svg" :startYear="2016" link="https://developer.mozilla.org/en-US/docs/Web/JavaScript"></skillBox>
+    <skillBox text="Vue" icon="src/assets/svg/skills/vue.svg" :startYear="2017" link="https://vuejs.org/"></skillBox>
+    <skillBox text="Firebase" icon="src/assets/svg/skills/firebase.svg" :startYear="2017" link="https://firebase.google.com/"></skillBox>
+    <skillBox text="Node" icon="src/assets/svg/skills/node.svg" :startYear="2016" link="https://nodejs.org/en/"></skillBox>
+    <skillBox text="Git" icon="src/assets/svg/skills/git.svg" :startYear="2016" link="https://git-scm.com/"></skillBox>
+    <skillBox text="C#" icon="src/assets/svg/skills/cSharp.svg" :startYear="2016" link="https://docs.microsoft.com/en-us/dotnet/csharp/"></skillBox>
+    <skillBox text="Unity" icon="src/assets/svg/skills/unity.svg" :startYear="2016" link="https://unity.com/"></skillBox>
+    <skillBox text="Godot" icon="src/assets/svg/skills/godot.svg" :startYear="2020" link="https://godotengine.org/"></skillBox>
+    <skillBox text="Affinity" icon="src/assets/svg/skills/affinityDesigner.svg" :startYear="2018" link="https://affinity.serif.com/en-us/designer/"></skillBox>
   </div>
 </div>
 </template>
 
+<script setup lang="ts">
+import SkillBox from '@/components/SkillBox.vue';
+</script>
 
 <style scoped>
 .skills {
@@ -78,11 +37,6 @@
   }
 }
 
-h3 {
-  font-weight: bold;
-  margin-top: 8px;
-}
-
 
 .list {
   display: flex;
@@ -91,39 +45,6 @@ h3 {
   align-items: center;
   flex-wrap: wrap;
   margin-top: 30px;
-}
-
-.skill {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 20px;
-  background-color: var(--col-old-pencil);
-  margin: 10px;
-  width: 150px;
-  height: 150px;
-  border-radius: 10px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  color: var(--col-old-paper);
-  transition: all 0.2s;
-  cursor: pointer;
-}
-
-.time {
-  color: var(--col-orange);
-}
-
-@media (hover: hover) {
-  .skill:hover {
-    box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
-    transform: scale(1.1);
-    color: var(--col-old-paper) !important;
-  }
-}
-
-img {
-  max-width: 50px;
 }
 
 </style>
