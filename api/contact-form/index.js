9341327ee1dd632fdfff7073bbd900388fe9f787
zeoxo <dyldougherty@gmail.com>
2022-09-07 18:05:27 -0400

    updated api headers

--- api/contact-form/index.js
+++ api/contact-form/index.js
@@ -7,7 +7,7 @@ const app = express()
 const cors = require('cors');
 app.use (express.json());
 app.use(cors({
-  origin: ['*', 'http://127.0.0.1:5173']
+  origin: ['*', 'http://127.0.0.1:5173, drd.software']
 }))
  
 app.get('/', (req, res) => res.send('Hello World!'))
